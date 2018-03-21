df55b572df56381ca7f441fcc6a583650c2a9c41
rosen <rosen369@outlook.com>
2018-03-21 15:07:29 +0800

    remove vue

--- src/Web/wwwroot/ts/viewModel.js
+++ src/Web/wwwroot/ts/viewModel.js
@@ -1,1 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ViewModel = /** @class */ (function () {
+    function ViewModel() {
+    }
+    return ViewModel;
+}());
+exports.ViewModel = ViewModel;
 //# sourceMappingURL=viewModel.js.map
